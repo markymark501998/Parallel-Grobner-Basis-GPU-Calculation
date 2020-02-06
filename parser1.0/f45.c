9850144b7347053ef878004e98e834d50cff5ab2
jakeosh17 <jakeposh17@gmail.com>
2020-02-06 12:52:54 -0500

    f45

---
+++ parser1.0/f45.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "poly.h"
+#include "macaulay.h"
+
+struct PolynomialSystem *f45(struct PolynomialSystem *input) {
+
+  struct LabelledPolynomial *L;
+}
---
+++ parser1.0/f45.h
@@ -0,0 +1,4 @@
+struct LabelledPolynomial {
+  int i;
+  struct Polynomial *poly;
+};
